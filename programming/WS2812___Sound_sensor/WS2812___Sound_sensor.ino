4c6e2c5b510fdc3f9890d29c127e25396217e4e5
fenil <fenil.c999@gmail.com>
2024-01-04 12:08:35 +0530

    individual code for all the sensors

---
+++ programming/WS2812___Sound_sensor/WS2812___Sound_sensor.ino
@@ -0,0 +1,74 @@
+/*
+VCC = 5V
+DATA = 6
+GROUND = GND 
+ */
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+int soundsensor = A0;
+
+int soundsensorState = 0;
+
+void setup() {
+
+ pinMode(soundsensor, INPUT);
+
+   Serial.begin(9600);
+  
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+
+  soundsensorState = analogRead(soundsensor);
+
+    Serial.println(soundsensorState);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (soundsensorState >= 600) {
+    // turn LED on:
+    colorWipe(strip.Color(255, 0, 0), 50); // Red;
+  } else {
+    // turn LED off:
+    colorWipe(strip.Color(0, 0, 0), 0); // off;
+  }
+
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
