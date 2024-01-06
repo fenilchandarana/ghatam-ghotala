a6b86f421888531a3101343f9202b8c61d98c079
fenil <fenil.c999@gmail.com>
2024-01-06 19:18:52 +0530

    sdcard module tf

---
+++ programming/SD_card_TF_rxtx___Ultrasonic_sensor/SD_card_TF_rxtx___Ultrasonic_sensor.ino
@@ -0,0 +1,44 @@
+#define trigPin 5
+#define echoPin 4
+
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+// Use pins 2 and 3 to communicate with DFPlayer Mini
+static const uint8_t PIN_MP3_TX = 3; // Connects to module's RX
+static const uint8_t PIN_MP3_RX = 2; // Connects to module's TX
+SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
+
+// Create the Player object
+DFRobotDFPlayerMini player;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+    softwareSerial.begin(9600);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+
+    if (player.begin(softwareSerial) && (distance >= 20)) {
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+        player.volume(15);
+    player.play(3);
+  }
+  delay(500);
+}
